6f55f97a04cc828db00bdd6b4fc8539b1af9b867
Adelar da Silva Queiróz <adelarsq@gmail.com>
2022-10-08 16:01:26 -0300

    Update image_preview.lua

--- lua/image_preview.lua
+++ lua/image_preview.lua
@@ -1,1 +1,54 @@
-a
+
+local M = {}
+
+-- local function GetFileName(url)
+--   return url:match("^.+/(.+)$")
+-- end
+
+local function GetFileExtension(url)
+  return url:match("^.+(%..+)$")
+end
+
+local function IsImage(url)
+    local extension = GetFileExtension(url)
+
+    if extension == '.bmp' then
+        return true
+    elseif extension == '.jpg' or extension == '.jpeg' then
+        return true
+    elseif extension == '.png' then
+        return true
+    end
+
+    return false
+end
+
+
+function M.PreviewImage()
+    local use, imported = pcall(require, "nvim-tree.lib")
+    if use then
+        local absolutePath = imported.get_node_at_cursor().absolute_path
+
+        if IsImage(absolutePath) then
+            local command = "silent !wezterm cli split-pane -- powershell wezterm imgcat "
+            command = command .. absolutePath
+            command = command .. " ; pause"
+
+            vim.api.nvim_command(command)
+        else
+            print("No preview for file "..absolutePath)
+        end
+    else
+        return ''
+    end
+end
+
+function M.setup()
+
+    local command = "au Filetype NvimTree nmap <buffer> <silent> <leader>p :lua require('image_preview').PreviewImage()<cr>"
+    vim.api.nvim_command(command)
+
+end
+
+return M
+
