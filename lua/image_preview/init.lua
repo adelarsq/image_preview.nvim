2d49e8aff523e920181fb5609a8349c033d3f1c6
Adelar da Silva Queiróz <adelarsq@gmail.com>
2023-05-23 18:04:45 -0300

    feat: add oil support

--- lua/image_preview/init.lua
+++ lua/image_preview/init.lua
@@ -1,4 +1,3 @@
-
 local M = {}
 
 -- local function GetFileName(url)
@@ -6,7 +5,7 @@ local M = {}
 -- end
 
 local function GetFileExtension(url)
-  return url:match("^.+(%..+)$")
+    return url:match("^.+(%..+)$")
 end
 
 local function IsImage(url)
@@ -25,28 +24,47 @@ local function IsImage(url)
     return false
 end
 
+function M.PreviewImage(absolutePath)
+    if IsImage(absolutePath) then
+        local command = ""
 
-function M.PreviewImage()
+        if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
+            command = "silent !wezterm cli split-pane -- powershell wezterm imgcat "
+            command = command .. absolutePath
+            command = command .. " ; pause"
+        else
+            command = "silent !wezterm cli split-pane -- bash -c 'wezterm imgcat "
+            command = command .. absolutePath
+            command = command .. " ; read'"
+        end
+
+        vim.api.nvim_command(command)
+    else
+        print("No preview for file " .. absolutePath)
+    end
+end
+
+function M.PreviewImageNvimTree()
     local use, imported = pcall(require, "nvim-tree.lib")
     if use then
         local absolutePath = imported.get_node_at_cursor().absolute_path
+        M.PreviewImage(absolutePath)
+    else
+        return ''
+    end
+end
 
-        if IsImage(absolutePath) then
-            local command = ""
+function M.PreviewImageOil()
+    local use, imported = pcall(require, "oil")
+    if use then
+        local entry = imported.get_cursor_entry()
 
-            if vim.fn.has('win32') == 1 or vim.fn.has('win64') == 1 then
-                command = "silent !wezterm cli split-pane -- powershell wezterm imgcat "
-                command = command .. absolutePath
-                command = command .. " ; pause"
-            else
-                command = "silent !wezterm cli split-pane -- bash -c 'wezterm imgcat "
-                command = command .. absolutePath
-                command = command .. " ; read'"
-            end
+        if (entry['type'] == 'file') then
+            local dir = imported.get_current_dir()
+            local fileName = entry['name']
+            local fullName = dir .. fileName
 
-            vim.api.nvim_command(command)
-        else
-            print("No preview for file "..absolutePath)
+            M.PreviewImage(fullName)
         end
     else
         return ''
@@ -54,11 +72,13 @@ function M.PreviewImage()
 end
 
 function M.setup()
-
-    local command = "au Filetype NvimTree nmap <buffer> <silent> <leader>p :lua require('image_preview').PreviewImage()<cr>"
+    local command =
+    "au Filetype NvimTree nmap <buffer> <silent> <leader>p :lua require('image_preview').PreviewImageNvimTree()<cr>"
     vim.api.nvim_command(command)
 
+    local command =
+    "au Filetype oil nmap <buffer> <silent> <leader>p :lua require('image_preview').PreviewImageOil()<cr>"
+    vim.api.nvim_command(command)
 end
 
 return M
-
